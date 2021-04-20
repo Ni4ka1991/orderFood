fafb8342570482f54bdc76a92937fb2aabc0e3d0
Ni4ka <astahova_nica@mail.ru>
2021-04-20 09:02:44 +0300

    Add files app.py restaurant.py

--- app.py
+++ app.py
@@ -7,7 +7,36 @@
 from db import *
 from restaurant import *
 
+food = load( 'food')
+order = {
+  "items": [],
+  
 
-food = load( 'food' )
-printItems( food, "MENU" )
+}
+
+
+while True:
+ action = printActionsMenu(
+    [
+       "Show food",
+       "Order food",
+       "Show order ", 
+       "Exit"
+    ],
+    "MAIN MENU" 
+ )
+ if( action == 1 ):
+   printItems( food, "TODAYS MENU" )
+ 
+ if( action == 2 ):
+   item_i = int( input( "Which item? >>> " )) - 1
+   item_q = int( input( f"How many < {food[item_i]['name']} > do you want? >>> " ))
+
+ if( action == 3):
+   printItems( order["items"], "YOUR ORDER" )
+   
+    
+
+ if( action == 4 ):
+   break
 
--- restaurant.py
+++ restaurant.py
@@ -3,13 +3,33 @@
 from os import system
 
 def printItems( items, title = None ):
-
+ 
  system( "clear" )
- print( "#"*30 )
- print( title ) 
- print( "#"*30 )
+
+ if( title != None ):
+  print( "#" * 30 )
+  print( title ) 
+ print( "#" * 30 )
  for i in range( len( items ) ):
      print(
      f"{i + 1:2} {items[i]['name']:15} {items[i]['price']['amount']:7.2f} { items[i]['price']['currency']} "
- )
- print( "#"*30 )
+  )
+ print( "#" * 30 )
+ input( "hit ENTER to continue ..." )
+
+
+def printActionsMenu( items, title = None ):
+ 
+ system( "clear" )
+ if( title != None ):
+  print( "#" * 20 )
+  print( title ) 
+ print( "#" * 20 )
+ for i in range( len( items ) ):
+     print(
+     f"{i + 1:2} {items[i]} "
+  )
+ print( "#" * 20 )
+ 
+ option = int( input( ">>> " ))
+ return option
